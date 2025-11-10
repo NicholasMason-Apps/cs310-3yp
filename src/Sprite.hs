cdab71e6a07cad215b8085f562e1f9b181e2e8f8
Nicholas Mason-Apps <nicholas.mason-apps@live.co.uk>
2025-11-10 15:02:45 +0000

    Fixed mistakes in boundary box checking

--- src/Sprite.hs
+++ src/Sprite.hs
@@ -47,9 +47,10 @@ blockPlayer :: Float -> System' ()
 --         let (Position (V2 _ y''')) = posP'''
 --         return $ if right then Position (V2 (xw + (fromIntegral ww / 2) + (fromIntegral wp / 2)) y''') else posP'''
 blockPlayer t = cmapM $ \(Player, Position posP, Velocity (V2 vx vy), spriteP) -> do
-    let (Position tempPos) = stepPositionFormula t (Position posP) (Velocity (V2 vx vy))
+    -- let (Position tempPos) = stepPositionFormula t (Position posP) (Velocity (V2 vx vy))
     cfold (\acc (Wall, Position posW, spriteW) ->
         let
+            (Position tempPos) = stepPositionFormula t (Position posP) acc
             top = checkBoundaryBoxTopIntersection tempPos spriteP posW spriteW
             bottom = checkBoundaryBoxBottomIntersection tempPos spriteP posW spriteW
             left = checkBoundaryBoxLeftIntersection tempPos spriteP posW spriteW
@@ -99,56 +100,56 @@ stepAnimations dT = do
 -- Note: Sprite positions are centered based on their Position component
 checkBoundaryBoxTopIntersection :: V2 Float -> Sprite -> V2 Float -> Sprite -> Bool
 checkBoundaryBoxTopIntersection (V2 x1 y1) s1 (V2 x2 y2) s2 =
-    top1 < bottom2 && bottom1 > bottom2 && right1 > left2 && left1 < right2
+    bottom1 < top2 && top1 > top2 && right1 > left2 && left1 < right2
     where
         (w1,h1) = spriteDimensions s1
         (w2,h2) = spriteDimensions s2
         left1 = x1 - fromIntegral w1/2
         right1 = x1 + fromIntegral w1/2
-        top1  = y1 - fromIntegral h1/2
-        bottom1 = y1 + fromIntegral h1/2
+        top1  = y1 + fromIntegral h1/2
+        bottom1 = y1 - fromIntegral h1/2
         left2 = x2 - fromIntegral w2/2
         right2 = x2 + fromIntegral w2/2
-        bottom2 = y2 + fromIntegral h2/2
+        top2 = y2 + fromIntegral h2/2
 checkBoundaryBoxBottomIntersection :: V2 Float -> Sprite -> V2 Float -> Sprite -> Bool
 checkBoundaryBoxBottomIntersection (V2 x1 y1) s1 (V2 x2 y2) s2 =
-    bottom1 > top2 && top1 < top2 && right1 > left2 && left1 < right2
+    top1 > bottom2 && bottom1 < bottom2 && right1 > left2 && left1 < right2
     where
         (w1,h1) = spriteDimensions s1
         (w2,h2) = spriteDimensions s2
         left1 = x1 - fromIntegral w1/2
         right1 = x1 + fromIntegral w1/2
-        top1  = y1 - fromIntegral h1/2
-        bottom1 = y1 + fromIntegral h1/2
+        top1  = y1 + fromIntegral h1/2
+        bottom1 = y1 - fromIntegral h1/2
         left2 = x2 - fromIntegral w2/2
         right2 = x2 + fromIntegral w2/2
-        top2  = y2 - fromIntegral h2/2
+        bottom2 = y2 - fromIntegral h2/2
 checkBoundaryBoxLeftIntersection :: V2 Float -> Sprite -> V2 Float -> Sprite -> Bool
 checkBoundaryBoxLeftIntersection (V2 x1 y1) s1 (V2 x2 y2) s2 =
-    right1 > left2 && left1 < left2 && bottom1 > top2 && top1 < bottom2
+    right1 > left2 && left1 < left2 && bottom1 < top2 && top1 > bottom2
     where
         (w1,h1) = spriteDimensions s1
         (w2,h2) = spriteDimensions s2
         left1 = x1 - fromIntegral w1/2
         right1 = x1 + fromIntegral w1/2
-        top1  = y1 - fromIntegral h1/2
-        bottom1 = y1 + fromIntegral h1/2
+        top1  = y1 + fromIntegral h1/2
+        bottom1 = y1 - fromIntegral h1/2
         left2 = x2 - fromIntegral w2/2
-        top2  = y2 - fromIntegral h2/2
-        bottom2 = y2 + fromIntegral h2/2
+        top2  = y2 + fromIntegral h2/2
+        bottom2 = y2 - fromIntegral h2/2
 checkBoundaryBoxRightIntersection :: V2 Float -> Sprite -> V2 Float -> Sprite -> Bool
 checkBoundaryBoxRightIntersection (V2 x1 y1) s1 (V2 x2 y2) s2 =
-    left1 < right2 && right1 > right2 && bottom1 > top2 && top1 < bottom2
+    left1 < right2 && right1 > right2 && bottom1 < top2 && top1 > bottom2
     where
         (w1,h1) = spriteDimensions s1
         (w2,h2) = spriteDimensions s2
         left1 = x1 - fromIntegral w1/2
         right1 = x1 + fromIntegral w1/2
-        top1  = y1 - fromIntegral h1/2
-        bottom1 = y1 + fromIntegral h1/2
+        top1  = y1 + fromIntegral h1/2
+        bottom1 = y1 - fromIntegral h1/2
         right2 = x2 + fromIntegral w2/2
-        top2  = y2 - fromIntegral h2/2
-        bottom2 = y2 + fromIntegral h2/2
+        top2  = y2 + fromIntegral h2/2
+        bottom2 = y2 - fromIntegral h2/2
 
 loadStaticSprite :: FilePath -> Picture
 loadStaticSprite path = let
